d53d8b81edc959826a420d91f09fc03146513c63
Cesar <cesar.ayuso@campusaula.com>
2025-05-19 13:57:31 +0200

    Primera tocada

--- aulario/urls.py
+++ aulario/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("usuarios/", views.UserViewSet.list, name="usuarios"),
-    path("usuarios/<int:pk>/", views.UserViewSet.detail, name="usuario"),
+    #path("usuarios/<int:pk>/", views.UserViewSet.detail, name="usuario"),
 ]
