62e5d28212553aa85e87b64a5a8db5e4a936f92d
cristian <cbossaers@icloud.com>
2025-05-19 13:21:24 +0200

    cesar

---
+++ aulario/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("usuarios/", views.UserViewSet.list, name="usuarios"),
+    path("usuarios/<int:pk>/", views.UserViewSet.detail, name="usuario"),
+]
